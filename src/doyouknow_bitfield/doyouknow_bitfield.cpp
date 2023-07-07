06026e4e45afe5c29e1bb8ff6dba3ba9a8e49ea4
taebin.heo <taebin.heo@konantech.com>
2023-07-07 15:20:42 +0900

    [Modify] bitfield

--- src/doyouknow_bitfield/doyouknow_bitfield.cpp
+++ src/doyouknow_bitfield/doyouknow_bitfield.cpp
@@ -24,6 +24,7 @@ void DOYOUKNOW_CLASS::bitfield(void)
 		bitfield_test01 test_01;
 		memset(&test_01, 0, sizeof(struct bitfield_test01));
 		printf("test_bf %p\n", &test_01);
+		printf("test_bf size %lld\n", (int64_t)sizeof(struct bitfield_test01)); // 1 byte
 		// (uint8_t)1 = 0000 0001
 		// (uint8_t)2 = 0000 0010
 		test_01.a = (uint8_t)1; // save 1bit : 1 (bit position 1)
@@ -39,6 +40,7 @@ void DOYOUKNOW_CLASS::bitfield(void)
 		bitfield_test02 test_02;
 		memset(&test_02, 0, sizeof(struct bitfield_test02));
 		printf("test_bf %p\n", &test_02);
+		printf("test_bf size %lld\n", (int64_t)sizeof(struct bitfield_test02)); // 4 byte
 		// (uint32_t)1 = 0000 0000 0000 0000 0000 0000 0000 0001
 		// (uint32_t)2 = 0000 0000 0000 0000 0000 0000 0000 0010
 		test_02.a = (uint32_t)1; // save 1bit : 1 (bit position 1)
@@ -55,9 +57,26 @@ void DOYOUKNOW_CLASS::bitfield(void)
 		bitfield_test03 test_03;
 		memset(&test_03, 0, sizeof(struct bitfield_test03));
 		printf("test_bf %p\n", &test_03);
+		printf("test_bf size %lld\n", (int64_t)sizeof(struct bitfield_test03)); // 4 byte
 		// (uint32_t)1 = 0000 0000 0000 0000 0000 0000 0000 0001
 		test_03.a = (uint32_t)1; // save 1bit : 1 (bit position 1)
 		test_03.b = (uint32_t)1; // save 1bit : 1 (bit position 8)
+
+		// Total size 8 byte (uint32_t x 2)
+		struct bitfield_test04
+		{
+			uint32_t a : 1; // bit position 1
+			uint32_t   : 0; // this mean, uint32_t x 2, must be unnamed.
+			uint32_t b : 1; // next 4 byte, bit position 1
+			uint32_t   : 30; // Padding
+		};
+		bitfield_test04 test_04;
+		memset(&test_04, 0, sizeof(struct bitfield_test04));
+		printf("test_bf %p\n", &test_04);
+		printf("test_bf size %lld\n", (int64_t)sizeof(struct bitfield_test04)); // 8 byte
+		// (uint32_t)1 = 0000 0000 0000 0000 0000 0000 0000 0001
+		test_04.a = (uint32_t)1; // save 1bit : 1 (bit position 1)
+		test_04.b = (uint32_t)1; // save 1bit : 1 (next 4 byte, bit position 1)
 
 		return;
 
